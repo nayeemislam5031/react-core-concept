54de5d6271df6adbd98a0f00c6ecdd51868300d9
nayeemislam5031 <you@example.comnayeemislam5031@gmail.com>
2022-01-24 20:59:46 +0600

    second

--- src/App.js
+++ src/App.js
@@ -2,23 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  var style={
+    color:'green',
+    backgroundColor:'pink',
+    borderRadius:'1rem',
+    padding:'3px '
+
+  }
+
+  var person ={
+    name: "Dr. Mahafurjur Rahaman",
+    job: "Doctor"
+
+  } 
+  var person2 ={
+    name:"Eva Rahaman",
+    job:"Student"
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>I am a React person</p>
+        <Person> </Person>
+       
+        
       </header>
     </div>
+  );
+}
+
+function Person(){
+  return (
+    <h1>Name: Sakib al Hasan</h1>
   );
 }
 
